86def7f653b80e0d4e6e94137def589d6b1626cb
Turra <joao.t06@hotmail.com>
2023-05-05 19:03:54 -0300

    class 8

--- utils.py
+++ utils.py
@@ -9,3 +9,24 @@ def generate_random_int_array(size, scaller = 1, offset = 0):
 
 def generate_gaussian_noise(shape, scaller = 1):
     return np.random.randn(shape)*scaller
+
+def zscore_normalize(X):
+    """
+    Normaliza todas as colunas em X
+    
+    Argumentos:
+      X (ndarray (m,n))     : Dados de entrada, m amostras (linhas), n características (colunas)
+      
+    Retorna:
+      X_norm (ndarray (m,n)): Matriz X normalizada
+      mu (ndarray (n,))     : média de cada característica
+      sigma (ndarray (n,))  : desvio padrão de cada característica
+    """
+    # Encontrando a média de cada característica/coluna
+    mu     = np.mean(X, axis=0)                 # mu terá shape (n,) ---> axis=0 indica que a operação será feita ao longo das linhas, para cada coluna
+    # Encontrando o desvio padrão de cada característica/coluna
+    sigma  = np.std(X, axis=0)                  # sigma will have shape (n,)
+    # O comando abaixo subtrai a média mu de cada coluna para cada exemplo, e divide pelo desvio correspondente
+    X_norm = (X - mu) / sigma      
+
+    return (X_norm, mu, sigma)
--- utils_reg_linear.py
+++ utils_reg_linear.py
@@ -79,6 +79,33 @@ def calcula_gradiente(x, y, w, b):
         
     return dj_dw, dj_db
 
+def calcula_gradiente_vetorizado(X, y, w, b): 
+    """
+    Calcula Gradiente para Regressão Linear
+    Args:
+      X (ndarray (m,n)): Dados, contendo m exemplos com n características
+      y (ndarray (m,)) : valores alvo
+      w (ndarray (n,)) : parâmetros w do modelo  
+      b (scalar)       : parâmetro b do modelo
+      
+    Retorna:
+      dj_dw (ndarray (n,)): O gradiente da função custo com relação aos parâmetros w. 
+      dj_db (escalar):      O gradiente da função custo com relação ao parâmetro b. 
+    """
+    m,n = X.shape           #(número de exemplos, número de características)
+    dj_dw = np.zeros((n,))
+    dj_db = 0.
+
+    for i in range(m):                             
+        erro = (np.dot(X[i], w) + b) - y[i]   
+        for j in range(n):                         
+            dj_dw[j] = dj_dw[j] + erro * X[i, j]    
+        dj_db = dj_db + erro                        
+    dj_dw = dj_dw / m                                
+    dj_db = dj_db / m                                
+        
+    return dj_db, dj_dw
+
 def metodo_do_gradiente(x, y, w_in, b_in, alpha, num_iters, calcula_custo, calcula_gradiente): 
     """
     Aplica o Método do Gradiente para ajustar w,b. Atualiza w,b ao longo de 
@@ -126,4 +153,47 @@ def metodo_do_gradiente(x, y, w_in, b_in, alpha, num_iters, calcula_custo, calcu
  
     return w, b, J_history, p_history #retorna w,b e valores históricos
 
+def metodo_do_gradiente_vetorizado(X, y, w_in, b_in, calcula_custo, calcula_gradiente, alpha, num_iters): 
+    """
+    Performa Método do Gradiente para aprender theta. Atualiza theta ao longo de  
+    num_iters passos de iteração usando uma taxa de aprendizado alpha
+    
+    Args:
+      X (ndarray (m,n))   : Dados, contendo m exemplos com n características
+      y (ndarray (m,))    : valores alvo
+      w_in (ndarray (n,)) : valores iniciais dos parâmetros w do modelo  
+      b_in (escalar)      : valor inicial do parâmetro b do modelo
+      calcula_custo       : função que calcula o custo
+      calcula_gradiente   : função que calcula o gradiente
+      alpha (float)       : taxa de aprendizado
+      num_iters (int)     : Número de iterações para o método do gradiente
+      
+    Retorna:
+      w (ndarray (n,)) : Valores atualizados para os parâmetros w
+      b (scalar)       : Valores atualizado para o parâmetro b 
+      """
+    
+    # Valores históricos
+    J_history = []
+    w = w_in
+    b = b_in
+    
+    for i in range(num_iters):
+
+        # Calcula o gradiente
+        dj_db,dj_dw = calcula_gradiente(X, y, w, b)   ##None
+
+        # Atualiza os parâmetros
+        w = w - alpha * dj_dw               ##None
+        b = b - alpha * dj_db               ##None
+      
+        # Salva o custo
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( calcula_custo(X, y, w, b))
+
+        # Faz print de tempos em tempos
+        if i% math.ceil(num_iters / 10) == 0:
+            print(f"Iteração {i:4d}: Custo {J_history[-1]:8.2f}   ")
+        
+    return w, b, J_history # retorna valores finais e históricos
 
